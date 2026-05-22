7d4dec3d29d96fc4fb5d1881268253de79325d4b
Anushka <anushkajagtap2212@gmail.com>
2026-05-22 23:02:16 +0530

    Best Time to Buy and Sell Stock

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,3 +1,25 @@
 public class BestTimetoBuyandSellStock {
-    
+    public int maxProfit(int[] prices) 
+    {
+        int minPrice = Integer.MAX_VALUE ;
+        int profit = 0;
+        for(int price : prices)
+        {
+            if(price < minPrice)
+            {
+                minPrice = price;
+            }
+            else
+            {
+                profit = Math.max(profit, price-minPrice);
+            }
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        BestTimetoBuyandSellStock solution = new BestTimetoBuyandSellStock();
+        int[] prices = {7,1,5,3,6,4};
+        int maxProfit = solution.maxProfit(prices);
+        System.out.println("Maximum profit: " + maxProfit);
+    }
 }
