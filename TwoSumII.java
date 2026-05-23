4e71de8ff0365247a474d6861cb1f2bd5f137d9b
Anushka <anushkajagtap2212@gmail.com>
2026-05-24 00:49:37 +0530

    Two Sum II

---
+++ TwoSumII.java
@@ -0,0 +1,30 @@
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+     int left = 0 ;
+     int right = numbers.length - 1;
+     while(left < right)
+        {
+            if(numbers[left] + numbers[right] == target)
+            {
+                return new int[]{left+1 , right+1};
+            }
+            else if(numbers[left] + numbers[right] < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return new int[]{-1 , -1};   
+    }
+    public static void main(String[] args) {
+        TwoSumII t = new TwoSumII();
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        int[] result = t.twoSum(numbers, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+}
+    
