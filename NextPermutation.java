575e205fce8452723138f1f65d77122479f1f32b
Anushka <anushkajagtap2212@gmail.com>
2026-05-27 21:51:44 +0530

    Next Permutation

---
+++ NextPermutation.java
@@ -0,0 +1,44 @@
+public class NextPermutation {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while(i >= 0 && nums[i+1] <= nums[i])
+        {
+            i--;
+        }   
+        if(i >= 0)
+        {
+            int j = nums.length -1;
+            while(nums[j] <= nums[i])
+            {
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+    }
+    public void swap(int[] nums , int i , int j)
+    {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void reverse(int nums[],int i)
+    {
+        int j = nums.length - 1;
+        while(i < j)
+        {
+            swap(nums,j,i);
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        NextPermutation solution = new NextPermutation();
+        solution.nextPermutation(nums);
+        for(int num : nums)
+        {
+            System.out.print(num + " ");
+        }
+    }
+}
